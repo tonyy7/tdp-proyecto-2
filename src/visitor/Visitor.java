72db290c1fa429c3e4ecfb5b009475e61246c448
Antonio Sevenants <anto.7up@gmail.com>
2022-10-12 18:13:18 -0300

    Agrega interfaz visitor

---
+++ src/visitor/Visitor.java
@@ -0,0 +1,28 @@
+package visitor;
+
+import criatura.Cuerpo;
+import criatura.Snake;
+import ente.alimento.Alimento1;
+import ente.alimento.Alimento2;
+import ente.alimento.Alimento3;
+import ente.alimento.Alimento4;
+import ente.alimento.Alimento5;
+import ente.pared.Pared;
+import ente.powerUp.PowerUp1;
+import ente.powerUp.PowerUp2;
+import ente.powerUp.PowerUp3;
+
+public interface Visitor {
+	
+	public void visitAlimento1(Alimento1 a);
+	public void visitAlimento2(Alimento2 a);
+	public void visitAlimento3(Alimento3 a);
+	public void visitAlimento4(Alimento4 a);
+	public void visitAlimento5(Alimento5 a);
+	public void visitSnake(Snake s);
+	public void visitCuerpo(Cuerpo c);
+	public void visitPowerUp1(PowerUp1 p);
+	public void visitPowerUp2(PowerUp2 p);
+	public void visitPowerUp3(PowerUp3 p);
+	public void visitPared(Pared p);
+}
