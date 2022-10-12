6b0e4384ca3cf7c2e2928d35853b1c86abc26892
Antonio Sevenants <anto.7up@gmail.com>
2022-10-12 19:30:22 -0300

    crecion gui, arreglo visitor

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,7 +1,28 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class GUI extends JFrame {
-
+	public GUI() {
+		getContentPane().setLayout(null);
+		
+		JPanel panelGrilla = new JPanel();
+		panelGrilla.setBounds(10, 11, 280, 239);
+		getContentPane().add(panelGrilla);
+		
+		JLabel lblPuntaje = new JLabel("puntaje");
+		lblPuntaje.setBounds(300, 11, 124, 55);
+		getContentPane().add(lblPuntaje);
+		
+		JLabel lblNewLabel = new JLabel("tiempo");
+		lblNewLabel.setBounds(300, 72, 124, 55);
+		getContentPane().add(lblNewLabel);
+		
+		JButton btnmenu = new JButton("menu");
+		btnmenu.setBounds(300, 138, 124, 46);
+		getContentPane().add(btnmenu);
+	}
 }
--- src/visitor/Visitor.java
+++ src/visitor/Visitor.java
@@ -14,15 +14,14 @@ import ente.powerUp.PowerUp3;
 
 public interface Visitor {
 	
-	public void visitAlimento1(Alimento1 a);
-	public void visitAlimento2(Alimento2 a);
-	public void visitAlimento3(Alimento3 a);
-	public void visitAlimento4(Alimento4 a);
-	public void visitAlimento5(Alimento5 a);
-	public void visitSnake(Snake s);
-	public void visitCuerpo(Cuerpo c);
-	public void visitPowerUp1(PowerUp1 p);
-	public void visitPowerUp2(PowerUp2 p);
-	public void visitPowerUp3(PowerUp3 p);
+	public int visitAlimento1(Alimento1 a);
+	public int visitAlimento2(Alimento2 a);
+	public int visitAlimento3(Alimento3 a);
+	public int visitAlimento4(Alimento4 a);
+	public int visitAlimento5(Alimento5 a);
+	public int visitCuerpo(Cuerpo c);
+	public int visitPowerUp1(PowerUp1 p);
+	public int visitPowerUp2(PowerUp2 p);
+	public int visitPowerUp3(PowerUp3 p);
 	public void visitPared(Pared p);
 }
