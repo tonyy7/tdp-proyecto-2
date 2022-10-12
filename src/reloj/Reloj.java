464fa09625bab2c504b28d9a872fa0f9a15429ba
Agustín <oyaagustin@gmail.com>
2022-10-12 19:11:26 -0300

    Reloj terminado :^)

--- src/reloj/Reloj.java
+++ src/reloj/Reloj.java
@@ -1,5 +1,31 @@
 package reloj;
 
-public class Reloj {
+import java.util.TimerTask;
 
+public class Reloj extends TimerTask{
+	protected int time;
+	
+	public Reloj() {
+		time = 0;
+	}
+	
+	public void run() {
+		while(true) {
+			try {
+				Thread.sleep(1000);
+				time++;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	public String getTime() {
+		int m = time/60;
+		int s = time%60; 
+		
+		return (String.format("%02d", m)+":"+String.format("%02d", s));
+	}
+	
 }
