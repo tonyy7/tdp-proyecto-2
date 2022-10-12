cb85f97a993d487bcf4ae5f1175b57f31769df09
saezb <saezb@192.168.0.5>
2022-10-12 17:25:43 -0300

    clase ente

--- src/ente/Ente.java
+++ src/ente/Ente.java
@@ -1,5 +1,22 @@
 package ente;
 
-public abstract class Ente {
+import Position.Position;
 
+public abstract class Ente {
+	protected Position posicion;
+	protected EnteGrafico grafico;
+	
+	public Position getPosicion() {
+		return posicion;
+	}
+	public void setPosicion(Position posicion) {
+		this.posicion = posicion;
+	}
+	public EnteGrafico getGrafico() {
+		return grafico;
+	}
+	public void setGrafico(EnteGrafico grafico) {
+		this.grafico = grafico;
+	}
+	
 }
---
+++ src/ente/EnteGrafico.java
@@ -0,0 +1,9 @@
+package ente;
+
+public class EnteGrafico{
+	protected String url;
+	public EnteGrafico(String url){
+		this.url=url;
+	}
+	public String getUrl() {return this.url;}
+}
