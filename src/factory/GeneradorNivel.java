e8b93ff635bbfd22df3af4f2d55b951ac8f5b9a6
Agustín <oyaagustin@gmail.com>
2022-10-13 18:26:59 -0300

    Se corrigieron bugs del generador de niveles y se agregaron otros para arreglar mas tarde

--- src/factory/GeneradorNivel.java
+++ src/factory/GeneradorNivel.java
@@ -32,82 +32,82 @@ public class GeneradorNivel implements FactoryLevel {
 		try {
 			level = Files.readString(filePath);
 			level = level.replace("\n", "");
-			int x;
-			int y;
+			String x;
+			String y;
 			for(int i = 0; i < level.length(); i++) {
 				
 				switch(level.charAt(i)){
 				
 					case 'p':{
-						x = level.charAt(i+2);
-						y = level.charAt(i+2);
-						Pared p = new Pared(new Position(x,y));
-						miNivel[x][y].addFirst(p);
-						i=i+5;
+						x = Integer.toString(level.charAt(i+2)) + level.charAt(i+3);
+						y = Integer.toString(level.charAt(i+5)) + level.charAt(i+6);
+						Pared p = new Pared(new Position(Integer.parseInt(x),Integer.parseInt(y)));
+						miNivel[p.getPosition().getX()][p.getPosition().getY()].addFirst(p);
+						i=i+6;
 					}break;
 				
 					case '1':{
-						x = level.charAt(i+2);
-						y = level.charAt(i+2);
-						Alimento1 a = new Alimento1(new Position(x,y));
+						x = Integer.toString(level.charAt(i+2)) + level.charAt(i+3);
+						y = Integer.toString(level.charAt(i+5)) + level.charAt(i+6);
+						Alimento1 a = new Alimento1(new Position(Integer.parseInt(x),Integer.parseInt(y)));
 						consumibles.add(a);
-						i=i+5;
+						i=i+6;
 					}break;
 				
 					case'2':{
-						x = level.charAt(i+2);
-						y = level.charAt(i+2);
-						Alimento2 a = new Alimento2(new Position(x,y));
+						x = Integer.toString(level.charAt(i+2)) + level.charAt(i+3);
+						y = Integer.toString(level.charAt(i+5)) + level.charAt(i+6);
+						Alimento2 a = new Alimento2(new Position(Integer.parseInt(x),Integer.parseInt(y)));
 						consumibles.add(a);
-						i=i+5;
+						i=i+6;
 					}break;
 				
 					case'3':{
-						x = level.charAt(i+2);
-						y = level.charAt(i+2);
-						Alimento3 a = new Alimento3(new Position(x,y));
+						x = Integer.toString(level.charAt(i+2)) + level.charAt(i+3);
+						y = Integer.toString(level.charAt(i+5)) + level.charAt(i+6);
+						Alimento3 a = new Alimento3(new Position(Integer.parseInt(x),Integer.parseInt(y)));
 						consumibles.add(a);
-						i=i+5;
+						i=i+6;
 					}break;
 				
 					case'4':{
-						x = level.charAt(i+2);
-						y = level.charAt(i+2);
-						Alimento4 a = new Alimento4(new Position(x,y));
+						x = Integer.toString(level.charAt(i+2)) + level.charAt(i+3);
+						y = Integer.toString(level.charAt(i+5)) + level.charAt(i+6);
+						Alimento4 a = new Alimento4(new Position(Integer.parseInt(x),Integer.parseInt(y)));
 						consumibles.add(a);
-						i=i+5;
+						i=i+6;
 					}break;
 				
 					case'5':{
-						x = level.charAt(i+2);
-						y = level.charAt(i+2);
-						Alimento5 a = new Alimento5(new Position(x,y));
+						x = Integer.toString(level.charAt(i+2)) + level.charAt(i+3);
+						y = Integer.toString(level.charAt(i+5)) + level.charAt(i+6);
+						Alimento5 a = new Alimento5(new Position(Integer.parseInt(x),Integer.parseInt(y)));
 						consumibles.add(a);
-						i=i+5;
+						i=i+6;
 					}break;
 				
 					case'6':{
-						x = level.charAt(i+2);
-						y = level.charAt(i+2);
-						PowerUp1 pu = new PowerUp1(new Position(x,y));
+						x = Integer.toString(level.charAt(i+2)) + level.charAt(i+3);
+						y = Integer.toString(level.charAt(i+5)) + level.charAt(i+6);
+						PowerUp1 pu = new PowerUp1(new Position(Integer.parseInt(x),Integer.parseInt(y)));
 						consumibles.add(pu);
-						i=i+5;
+						i=i+6;
 					}break;
 				
 					case'7':{
-						x = level.charAt(i+2);
-						y = level.charAt(i+2);
-						PowerUp2 pu = new PowerUp2(new Position(x,y));
+						x = Integer.toString(level.charAt(i+2)) + level.charAt(i+3);
+						y = Integer.toString(level.charAt(i+5)) + level.charAt(i+6);
+						PowerUp2 pu = new PowerUp2(new Position(Integer.parseInt(x),Integer.parseInt(y)));
 						consumibles.add(pu);
-						i=i+5;
+						i=i+6;
 					}break;
 				
 					case'8':{
-						x = level.charAt(i+2);
-						y = level.charAt(i+2);
-						PowerUp3 pu = new PowerUp3(new Position(x,y));
+						x = Integer.toString(level.charAt(i+2)) + level.charAt(i+3);
+						y = Integer.toString(level.charAt(i+5)) + level.charAt(i+6);
+						PowerUp3 pu = new PowerUp3(new Position(Integer.parseInt(x),Integer.parseInt(y)));
 						consumibles.add(pu);
-						i=i+5;
+						i=i+6;
 					}break;
 				}
 			}
